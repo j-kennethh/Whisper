e903977f0e8944e8f32ed08ab09ac2334573d672
John Kenneth <johnkennethagpaoa@gmail.com>
2023-05-19 13:45:36 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+
+app.get("/register", (req, res) => {
+    res.render("register");
+});
+
+
+app.listen(3000, () => {
+    console.log("server running on port 3000");
+});
