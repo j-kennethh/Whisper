4aa9b07465fa7b188893c7e085e0ecd3ea01ccfd
John Kenneth <johnkennethagpaoa@gmail.com>
2023-05-19 14:18:03 +0800

    register users with username and password

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String
+});
+
+const User = mongoose.model("User", userSchema);  
 
 
 app.get("/", (req, res) => {
@@ -19,8 +28,35 @@ app.get("/login", (req, res) => {
 });
 
 
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email: username}).then((foundUser) => {
+        if (foundUser) {
+            if (foundUser.password === password) {
+                res.render("secrets");
+            }
+        }
+    });
+});
+
+
 app.get("/register", (req, res) => {
     res.render("register");
+});
+
+
+app.post("/register", (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save().then((status) => {
+        console.log(status);
+        res.render("secrets");
+    });
 });
 
 
